4c15494cd62774460d7a0a33c49adebe218a9b5a
AnjaliTestYantra <anjali.paleth@gmail.com>
2024-10-18 12:52:09 +0530

    added contactwithorg

---
+++ For_Jenkins/src/test/java/contactModule/CreateContactWithOrgTest.java
@@ -0,0 +1,11 @@
+package contactModule;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateContactWithOrgTest {
+	@Test
+	public void createContactWithOrg() {
+		 Reporter.log("Created Contact with Org successfully", true);
+		}
+}
