c7cb088dd19852e257e233976c58900c8205d8b6
PiyushAhir21 <vasudevahir21@gmail.com>
2025-03-10 17:02:10 +0530

    10-03

--- For_Jenkins/src/test/java/orgModule/CreateOrganizationTest.java
+++ For_Jenkins/src/test/java/orgModule/CreateOrganizationTest.java
@@ -6,14 +6,14 @@ import org.testng.annotations.Test;
 public class CreateOrganizationTest {
 	@Test(groups = "SmokeTest")
 	public void createOrg() {
-		 Reporter.log("Created Organization successfully", true);
+		Reporter.log("Created Organization successfully", true);
 		String URL = System.getProperty("Url");
 		String BROWSER = System.getProperty("Browser");
 		String UNAME = System.getProperty("Username");
 		String PWD = System.getProperty("Password");
-		System.out.println("Url is: "+URL);
-		System.out.println("Browser is: "+BROWSER);
-		System.out.println("Username is: "+UNAME);
-		System.out.println("Password is: "+PWD);
-		}
+		System.out.println("Url is: " + URL);
+		System.out.println("Browser is: " + BROWSER);
+		System.out.println("Username is: " + UNAME);
+		System.out.println("Password is: " + PWD);
+	}
 }
