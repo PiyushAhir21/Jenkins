8d234dab1079fe460972df32bdcd943418c398bd
PiyushAhir21 <vasudevahir21@gmail.com>
2025-03-11 14:36:11 +0530

    ...

--- For_Jenkins/src/test/java/contactModule/CreateContact.java
+++ For_Jenkins/src/test/java/contactModule/CreateContact.java
@@ -4,21 +4,19 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class CreateContact {
+	
 	@Test(groups = "RegressionTest")
-	public void createContactWithOrg() {
-		 Reporter.log("Created Contact with Org successfully", true);
-			Reporter.log("Created Organization successfully", true);
-			
-			
-			String URL = System.getProperty("Url");
-			String BROWSER = System.getProperty("Browser");
-			String UNAME = System.getProperty("Username");
-			String PWD = System.getProperty("Password");
-			
-			
-			System.out.println("Url is: " + URL);
-			System.out.println("Browser is: " + BROWSER);
-			System.out.println("Username is: " + UNAME);
-			System.out.println("Password is: " + PWD);
-		}
+	public void createContact() {
+		Reporter.log("Created Contact successfully", true);
+
+		String URL = System.getProperty("Url");
+		String BROWSER = System.getProperty("Browser");
+		String UNAME = System.getProperty("Username");
+		String PWD = System.getProperty("Password");
+
+		System.out.println("Url is: " + URL);
+		System.out.println("Browser is: " + BROWSER);
+		System.out.println("Username is: " + UNAME);
+		System.out.println("Password is: " + PWD);
+	}
 }
--- For_Jenkins/src/test/java/orgModule/CreateOrganizationTest.java
+++ For_Jenkins/src/test/java/orgModule/CreateOrganizationTest.java
@@ -7,14 +7,15 @@ public class CreateOrganizationTest {
 
 	@Test(groups = "SmokeTest")
 	public void createOrg() {
-		Reporter.log("Created Organization successfully", true);
+		Reporter.log("Created org successfully", true);
+
 		String URL = System.getProperty("Url");
 		String BROWSER = System.getProperty("Browser");
 		String UNAME = System.getProperty("Username");
 		String PWD = System.getProperty("Password");
+
 		System.out.println("Url is: " + URL);
 		System.out.println("Browser is: " + BROWSER);
 		System.out.println("Username is: " + UNAME);
-		System.out.println("Password is: " + PWD);
-	}
+		System.out.println("Password is: " + PWD);	}
 }
